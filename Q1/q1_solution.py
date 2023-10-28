ce5ba82a9ba3eb293cb37783cf24d340fd765395
Luis-FO <luisfernandophsc@gmail.com>
2023-10-28 11:06:28 -0300

    Configurando o Ambiente

---
+++ Q1/q1_solution.py
@@ -0,0 +1,4 @@
+# Seja a imagem satelital com ruído de fundo salt & pepper, inserida na pasta compartilhada 
+# denominada img46_gray_noise.png (pasta Questão 1). Implemente um código com alguma técnica 
+# para remoção parcial do ruído apresentado.
+
